9058e58ef8dfbb3e0fa244c3111ce157c5cedd90
User <user@example.com>
2026-03-17 13:52:43 +0200

    Updated base path for FirstBatch deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Calendar_Project/', // Required for GitHub Pages deployment
+  base: '/Calendar_Project_FirstBatch/', // Required for GitHub Pages deployment
 })
